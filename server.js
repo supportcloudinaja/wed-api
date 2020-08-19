7ecaccd7c54efbe3debd5ad85d75fcf95e4de259
Ibnu Bagus T <trisnandibagus@gmail.com>
2020-08-19 22:04:33 +0700

    add serverless module

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const serverless = require('serverless-http'); // add for serverless
 const express = require("express");
 const connectDB = require("./DB/Connection");
 const app = express();
+// app.use(express.urlencoded({
+//   extended: true
+// }));
+// app.use(express.json());
 const cors = require("cors");
 
 connectDB();
@@ -11,6 +16,6 @@ app.use(
   })
 );
 app.use("/api/wedding", require("./API/User"));
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => console.log("Server Started"));
+// app.listen(PORT, "0.0.0.0", () => console.log("Server Started"));
