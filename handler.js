87d5e4d7eecc16ee3ccbedf7da988216822bc993
Ibnu Bagus T <trisnandibagus@gmail.com>
2020-08-20 21:12:34 +0700

    enable cors and Access Control Allow Origin

--- handler.js
+++ handler.js
@@ -40,7 +40,8 @@ module.exports.getOne = (event, context, callback) => {
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
           headers: {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': "*"
           },
           body: 'Could not fetch the User.'
         }));
@@ -60,7 +61,8 @@ module.exports.getAll = (event, context, callback) => {
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
           headers: {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': "*"
           },
           body: 'Could not fetch the notes.'
         }))
