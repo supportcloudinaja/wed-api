14d81b6c0c73ec9d26edaecc7c48eaba1f66b93d
Ibnu Bagus T <trisnandibagus@gmail.com>
2020-08-22 18:26:33 +0700

    inu edit

--- handler.js
+++ handler.js
@@ -7,6 +7,7 @@ require('dotenv').config({
   path: './variables.env'
 });
 
+
 module.exports.create = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
@@ -15,12 +16,16 @@ module.exports.create = (event, context, callback) => {
       User.create(JSON.parse(event.body))
         .then(note => callback(null, {
           statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': "*"
+          },
           body: JSON.stringify(note)
         }))
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
           headers: {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': "*"
           },
           body: 'Could not create the User.'
         }));
@@ -32,9 +37,14 @@ module.exports.getOne = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      User.findById(event.pathParameters.id)
+      User.findById({
+          _id: event.pathParameters.id
+        })
         .then(note => callback(null, {
           statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': "*"
+          },
           body: JSON.stringify(note)
         }))
         .catch(err => callback(null, {
@@ -56,6 +66,9 @@ module.exports.getAll = (event, context, callback) => {
       User.find()
         .then(notes => callback(null, {
           statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': "*"
+          },
           body: JSON.stringify(notes)
         }))
         .catch(err => callback(null, {
@@ -74,17 +87,23 @@ module.exports.update = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      User.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), {
+      User.findByIdAndUpdate({
+          _id: event.pathParameters.id
+        }, JSON.parse(event.body), {
           new: true
         })
         .then(note => callback(null, {
           statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': "*"
+          },
           body: JSON.stringify(note)
         }))
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
           headers: {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': "*"
           },
           body: 'Could not fetch the notes.'
         }));
@@ -96,9 +115,14 @@ module.exports.delete = (event, context, callback) => {
 
   connectToDatabase()
     .then(() => {
-      User.findByIdAndRemove(event.pathParameters.id)
+      User.findByIdAndRemove({
+          _id: event.pathParameters.id
+        })
         .then(note => callback(null, {
           statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': "*"
+          },
           body: JSON.stringify({
             message: 'Removed note with id: ' + User._id,
             note: note
@@ -107,7 +131,8 @@ module.exports.delete = (event, context, callback) => {
         .catch(err => callback(null, {
           statusCode: err.statusCode || 500,
           headers: {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': "*"
           },
           body: 'Could not fetch the notes.'
         }));
